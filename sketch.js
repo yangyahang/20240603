d4901514cf2984aa37f149f0a4f0aaaa60d89d11
yangyahang <150992210+yangyahang@users.noreply.github.com>
2024-06-03 11:29:46 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -64,7 +64,6 @@ function drawSkeleton() {
     partA = pose.keypoints[0];
         partA = pose.keypoints[0];
     if (partA.score > 0.1) {
-      scale(-1, 1)
       text("412737206楊雅涵",partA.x,partA.y-200)
     }
     // eyes
